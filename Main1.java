a3ff093219905cef64713a3c268f86230ab0f225
spookyjjj <104603810+spookyjjj@users.noreply.github.com>
2022-06-28 17:08:19 +0900

    게임창 dialog로 변경

--- Main1.java
+++ Main1.java
@@ -6,11 +6,14 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Main1 extends JFrame{
+public class Main1 extends JDialog{
 	public Main1() {
+		setModal(true);
+		
 		JPanel pnl = new JPanel(null);
 		pnl.setPreferredSize(new Dimension(500, 500));
 		
@@ -32,6 +35,8 @@ public class Main1 extends JFrame{
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				//여기에 다시 로그인 창으로 돌아가게 하고싶다~~
+				//새로 로그인창으로 연결시키지 말고 그냥 게임창을 꺼버리면 되는거 아님???
+				dispose();
 			}
 		});
 		pnl.add(btn2);
@@ -39,10 +44,6 @@ public class Main1 extends JFrame{
 		pack();
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setResizable(false); //프레임크기 고정
-	}
-	public static void main(String[] args) {
-		new Main1().setVisible(true);
-
 	}
 
 }
